dbb29c181dd1aca090f94c0d349a96792cedc5ac
Salman Masood (Al-Farsi) <salmanmasood24@yahoo.com>
2021-08-14 03:25:03 +0500

    Develop - Commands Added

---
+++ Pattern.CQRS/Pattern.CQRS/Features/Players/Commands/UpdatePlayerCommand.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Pattern.CQRS.Services;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pattern.CQRS.Features.Players.Commands
+{
+    public class UpdatePlayerCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+        public int? ShirtNo { get; set; }
+        public string Name { get; set; }
+        public int? Appearances { get; set; }
+        public int? Goals { get; set; }
+
+        public class UpdatePlayerCommandHandler : IRequestHandler<UpdatePlayerCommand, int>
+        {
+            private readonly IPlayersService _playerService;
+
+            public UpdatePlayerCommandHandler(IPlayersService playerService)
+            {
+                _playerService = playerService;
+            }
+
+            public async Task<int> Handle(UpdatePlayerCommand command, CancellationToken cancellationToken)
+            {
+                var player = await _playerService.GetPlayerById(command.Id);
+                if (player == null)
+                    return default;
+
+                player.ShirtNo = command.ShirtNo;
+                player.Name = command.Name;
+                player.Appearances = command.Appearances;
+                player.Goals = command.Goals;
+
+                return await _playerService.UpdatePlayer(player);
+            }
+        }
+    }
+}
