5a041ac8c5545545708a593fe88f87a0d6d51ff7
Salman Masood (Al-Farsi) <salmanmasood24@yahoo.com>
2021-08-14 03:06:44 +0500

    Develop - Service Added

---
+++ Pattern.CQRS/Pattern.CQRS/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Pattern.CQRS.Models
+{
+    public partial class Player
+    {
+        public long Id { get; set; }
+        public int? ShirtNo { get; set; }
+        public string Name { get; set; }
+        public int? Appearances { get; set; }
+        public int? Goals { get; set; }
+    }
+}
